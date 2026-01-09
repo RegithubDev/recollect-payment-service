88d43d6f8e059a032f52e958ff0b73f456a0fbe8
Kunal Vartak <regithub10@gmail.com>
2026-01-09 15:20:47 +0530

    refactor(test): disable @Tests

--- src/test/java/com/example/walletmicroservice/WalletMicroserviceApplicationTests.java
+++ src/test/java/com/example/walletmicroservice/WalletMicroserviceApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.walletmicroservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WalletMicroserviceApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
